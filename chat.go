603e24d0c217755e6df66bb75820f4d5c284e28b
Glen Baker <iepathos@gmail.com>
2017-04-11 02:58:56 -0400

    Setup create message and test

--- chat.go
+++ chat.go
@@ -1,22 +1,101 @@
-// simple chat service
-import time
+package main // simple chat service
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	// "time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+
+	r "gopkg.in/gorethink/gorethink.v3"
+)
+
+// database name
+var DbName = os.Getenv("DBNAME")
+
+// table name for users service
+var TableName = "chat"
 
 // chat models
 type Room struct {
-	name string
-	users []string
+	Name  string   `json:"name"`
+	Users []string `json:"users"`
 }
 
 type Message struct {
-	username string
-	message string
-	timestamp time.Time
-	room string
+	Username string `json:"username"`
+	Message  string `json:"message"`
+	Room     string `json:"room"`
+	// Timestamp time.Time `json:"timestamp"`
 }
 
+// InsertMessage inserts a Message struct into rethinkdb
+func InsertMessage(message Message) {
+	// connect to db
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "localhost:28015",
+	})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	db := r.DB(DbName)
 
+	// insert user db
+	err = db.Table(TableName).Insert(map[string]interface{}{
+		"username": message.Username,
+		"message":  message.Message,
+		"room":     message.Room,
+		// "timestamp": message.Timestamp,
+	}).Exec(session)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
 
 // chat views
+func CreateMessage(w http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(io.LimitReader(req.Body, 1048576))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := req.Body.Close(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var message Message
+	err = json.Unmarshal(body, &message)
+	if err != nil {
+		w.WriteHeader(422)
+		log.Println(err.Error())
+	}
+
+	InsertMessage(message)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(message); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
 // create message
 // get messages
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/create", CreateMessage)
+	server := &http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+	log.Fatal(server.ListenAndServe())
+}
---
+++ chat_test.go
@@ -0,0 +1,126 @@
+// cat_test.go
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+
+	simplejson "github.com/bitly/go-simplejson"
+	r "gopkg.in/gorethink/gorethink.v3"
+)
+
+func createDatabase(databaseName string) {
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "localhost:28015",
+	})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Println("Creating database", databaseName)
+	_, err = r.DBCreate(databaseName).Run(session)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func createTable(tableName string) {
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "localhost:28015",
+	})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	db := r.DB("test")
+
+	log.Println("Creating table", tableName)
+	if _, err := db.TableCreate(tableName).RunWrite(session); err != nil {
+		log.Println(err)
+	}
+}
+
+func dropDatabase(databaseName string) {
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "localhost:28015",
+	})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Println("Dropping database", databaseName)
+	_, err = r.DBDrop(databaseName).Run(session)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func TestCreateMessage(t *testing.T) {
+	// lookup user in rethinkdb and make sure it now exists
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "localhost:28015",
+	})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	createDatabase("test")
+	createTable(TableName)
+
+	url := "/create"
+	jsonStr := []byte(`{"username":"Saitama","message":"herro","room":"onepunch"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(CreateMessage)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("CreateUser handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+
+	// Check the response body is what we expect.
+	reqJSON, err := simplejson.NewFromReader(rr.Body)
+	if err != nil {
+		t.Errorf("Error while reading request JSON: %s", err)
+	}
+	username := reqJSON.Get("username").MustString()
+	if username != "Saitama" {
+		t.Errorf("Expected request JSON response to have username Saitama")
+	}
+	message := reqJSON.Get("message").MustString()
+	if message != "herro" {
+		t.Errorf("Expected request JSON response to have message herro but got %s", message)
+	}
+	room := reqJSON.Get("room").MustString()
+	if room != "onepunch" {
+		t.Errorf("Expected request JSON response to have room onepunch but got %s", room)
+	}
+
+	db := r.DB("test")
+	cursor, err := db.Table(TableName).Count().Run(session)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	var count int
+	cursor.One(&count)
+	cursor.Close()
+	if count != 1 {
+		t.Errorf("Expected RethinkDB users table to have count of 1")
+	}
+	dropDatabase("test")
+}
